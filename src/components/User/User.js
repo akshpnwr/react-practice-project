afbbcb1a8cce456095facef7b96ceb9697ce1d1a
akshpnwr <akashpanwar9976@gmail.com>
2021-04-30 12:48:54 +0530

    adding conditional age input

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 
 const user = (props) => {
-  const users = props.inputs.map((input, i) => (
-    <Fragment key={i}>
-      <h3>{input.name}</h3>
-      <p>{input.age}</p>
-    </Fragment>
-  ));
+  const users = props.inputs.map((input, i) => {
+    return (
+      <Fragment key={i}>
+        <h3>{input.name}</h3>
+        <p>{+input.age < 1 ? 'Age should be > 0' : input.age}</p>
+      </Fragment>
+    );
+  });
 
   return <div>{users}</div>;
 };
